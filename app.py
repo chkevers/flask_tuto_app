3cfea54435d7fc003d479b132e70e17f46704785
chkevers <christophe.kevers@gmail.com>
2022-01-11 09:56:19 +0100

    Progressing

--- app.py
+++ app.py
@@ -9,4 +9,6 @@ def hello():
 
 
 if __name__=="__main__":
-    app.run(debug=True)   
+    app.run(debug=True, port=8080)   
+
+
---
+++ static/js/basic.js
@@ -0,0 +1,5 @@
+const hAlert = document.querySelector('h1');
+hAlert.addEventListener('mouseover', function(e) {
+    alert('Bouh')
+}
+);
