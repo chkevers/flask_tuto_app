7c3bc731c10c4e8ca1de8afb9956cede0c540833
chkevers <christophe.kevers@gmail.com>
2022-01-12 16:33:20 +0100

    Starting with data and jinja

--- app.py
+++ app.py
@@ -5,7 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    welcome = 'Welcome to this flask app'
+    return render_template('index.html', welcome=welcome)
+
+
+@app.route('/geomap')
+def geomap():
+    # item = 'Test for jinja'
+    return render_template('geomap.html', item='Test')
+
 
 
 if __name__=="__main__":
