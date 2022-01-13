a2aad74782bf18f6c696b7959109f8292b50c425
chkevers <christophe.kevers@gmail.com>
2022-01-13 15:22:09 +0100

    Starting with Leaflet

--- static/js/basic.js
+++
@@ -1,5 +0,0 @@
-// const hAlert = document.querySelector('h1');
-// hAlert.addEventListener('mouseover', function(e) {
-//     alert('Bouh')
-// }
-// );
---
+++ static/js/geomap.js
@@ -0,0 +1,31 @@
+const linkTest = document.querySelector('#map')
+linkTest.addEventListener('click', function(e) {
+    e.preventDefault;
+    alert('Here we go')
+});
+
+
+// Geomapping
+
+navigator.geolocation.getCurrentPosition(function success(pos){
+    console.log(pos);
+    const {latitude: lat, longitude: lgt} = pos.coords;
+    console.log(lat, lgt);
+
+    let map  = L.map('map').setView([lat, lgt], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([lat,lgt]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+
+
+
+},
+
+function error(err) {
+    console.log(err);
+}
+)
