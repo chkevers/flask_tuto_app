3ebe03c54be5a797c970660582062d06d6e326ef
chkevers <christophe.kevers@gmail.com>
2022-01-13 15:39:36 +0100

    bug fixing

--- static/js/geomap.js
+++ static/js/geomap.js
@@ -7,25 +7,25 @@
 
 // Geomapping
 
-navigator.geolocation.getCurrentPosition(function success(pos){
-    console.log(pos);
-    const {latitude: lat, longitude: lgt} = pos.coords;
-    console.log(lat, lgt);
+// navigator.geolocation.getCurrentPosition(function success(pos){
+//     console.log(pos);
+//     const {latitude: lat, longitude: lgt} = pos.coords;
+//     console.log(lat, lgt);
 
-    let map  = L.map('map').setView([lat, lgt], 13);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+//     let map  = L.map('map').setView([lat, lgt], 13);
+//     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//     }).addTo(map);
 
-    L.marker([lat,lgt]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+//     L.marker([lat,lgt]).addTo(map)
+//     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+//     .openPopup();
 
 
 
-},
+// },
 
-function error(err) {
-    console.log(err);
-}
-)
+// function error(err) {
+//     console.log(err);
+// }
+// )
